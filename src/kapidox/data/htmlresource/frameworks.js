e81bb695626b8d818a5243e8ee540aa03ef3c41f
Aurélien Gâteau <agateau@kde.org>
2014-05-09 17:28:51 +0200

    Nicer platform-note display

--- src/kapidox/data/htmlresource/frameworks.js
+++ src/kapidox/data/htmlresource/frameworks.js
@@ -43,9 +43,54 @@ function updatePlatforms() {
     });
 }
 
+var g_noteElement;
+var g_currentlyDescribedElement;
+
+function initNoteTip() {
+    g_noteElement = document.createElement("div");
+    $(g_noteElement).addClass("note-tip");
+    $(g_noteElement).html("<span class='note-tip-text'></span> <a class='note-close' href='#'>&#9447;</a>");
+
+    $("body").append(g_noteElement);
+
+    $(".note-close", g_noteElement).click(function() {
+        hideNoteTip();
+        return false;
+    });
+
+    $(".platform-note").click(function() {
+        if (g_currentlyDescribedElement == this) {
+            hideNoteTip();
+        } else {
+            var text = this.getAttribute("data-note");
+            showNoteTip(this, text);
+        }
+        return false;
+    });
+}
+
+function showNoteTip(anchorElement, text) {
+    g_currentlyDescribedElement = anchorElement;
+
+    var offset = $(anchorElement).offset();
+    var note = $(g_noteElement);
+    $(".note-tip-text", note).text(text);
+    note.css({
+            left: (offset.left + $(anchorElement).width() / 2 - note.outerWidth(true) / 2) + "px",
+            top: (offset.top - note.outerHeight(true) - 6) + "px"
+        })
+        .fadeIn();
+}
+
+function hideNoteTip() {
+    g_currentlyDescribedElement = null;
+    $(g_noteElement).fadeOut();
+}
+
 function main() {
     $("#showMaintainers").click(updateMaintainers);
     $("#platform-filter").click(updatePlatforms);
     $(".platform-checkbox").click(updatePlatforms);
+    initNoteTip();
     updateMaintainers();
 }
