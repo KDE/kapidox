952bcf96e3b6d263f2abd71bf099a372bd873c4f
Aurélien Gâteau <agateau@kde.org>
2014-05-22 18:27:20 +0200

    Avoid uncommon failures when running twice

--- src/kapidox/generator.py
+++ src/kapidox/generator.py
@@ -409,6 +409,13 @@ def generate_apidocs(ctx, tmp_dir, doxyfile_entries=None, keep_temp_dirs=False):
     # What about providing the build directory? We could get the version
     # as well, then.
 
+    if os.path.exists(ctx.htmldir):
+        # If we have files left there from a previous run but which are no
+        # longer generated (for example because a C++ class has been removed)
+        # then postprocess will fail because the left-over file has already been
+        # processed. To avoid that, we delete the html dir.
+        shutil.rmtree(ctx.htmldir)
+
     input_list = [ctx.srcdir]
     image_path_list = []
 
