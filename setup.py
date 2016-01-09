4f4d34c7ed36cfcc21e1801141f7b68431532fe5
l10n daemon script <scripty@kde.org>
2016-01-09 12:49:43 +0000

    Upgrade KF5 version to 5.19.0.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 
 setup(
         name='kapidox',
-        version='5.18.0',
+        version='5.19.0',
         description='KDE API documentation generation tools',
         maintainer = 'Alex Merry',
         maintainer_email = 'alex.merry@kde.org',
