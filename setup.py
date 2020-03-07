28e9da43db6c98b08722a1554e2552abd2c9a96f
l10n daemon script <scripty@kde.org>
2020-03-07 08:34:31 +0000

    GIT_SILENT Upgrade KF5 version to 5.68.0.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ for root, dirs, files in os.walk('src/kapidox/data/'):
 
 setup(
         name='kapidox',
-        version='5.69.0',
+        version='5.68.0',
         description='KDE API documentation generation tools',
         maintainer = 'Olivier Churlaud',
         maintainer_email = 'olivier@churlaud.com',
