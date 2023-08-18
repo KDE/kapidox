cf514dda288a0c7dcd2268b122eabae22d63b5b5
David Faure <faure@kde.org>
2023-08-18 18:59:03 +0200

    sigh

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='kapidox',
-        version='',
+        version='5.110.0',
         description='KDE API documentation generation tools',
         maintainer='Olivier Churlaud',
         maintainer_email='olivier@churlaud.com',
