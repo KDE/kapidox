40071dc06b49481e00d597ff13de7025cb7d28bf
l10n daemon script <scripty@kde.org>
2023-12-14 22:10:39 +0000

    GIT_SILENT Upgrade KF version to 5.114.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='kapidox',
-        version='5.113.0',
+        version='5.114.0',
         description='KDE API documentation generation tools',
         maintainer='Olivier Churlaud',
         maintainer_email='olivier@churlaud.com',
