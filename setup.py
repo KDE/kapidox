1c5c216af77865c66111bebb8a6f4cd31a7b0650
Jonathan Riddell <jr@jriddell.org>
2024-04-12 16:06:47 +0100

    update version for new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='kapidox',
-        version='6.1.0',
+        version='6.2.0',
         description='KDE API documentation generation tools',
         maintainer='Olivier Churlaud',
         maintainer_email='olivier@churlaud.com',
