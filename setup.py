fa1e07ac68c7b5513f8013de6fff19cc2bbb0156
Nicolas Fella <nicolas.fella@gmx.de>
2024-10-11 13:31:05 +0200

    Update version to 6.8.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='kapidox',
-        version='6.7.0',
+        version='6.8.0',
         description='KDE API documentation generation tools',
         maintainer='Olivier Churlaud',
         maintainer_email='olivier@churlaud.com',
